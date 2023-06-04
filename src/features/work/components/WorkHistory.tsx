e5dd62417d1fefd028a890420e1b24a886cdfc2d
Jarrod Malkovic <jarrodmalkovic@outlook.com>
2023-06-04 16:58:03 +1000

    :coffin: chore: remove unused code

--- src/features/work/components/WorkHistory.tsx
+++ src/features/work/components/WorkHistory.tsx
@@ -2,21 +2,6 @@ import { HomePageContentQuery } from 'graphql/generated';
 
 import { WorkHistoryItem } from './WorkHistoryItem';
 
-const workHistoryItems = [
-	{
-		title: 'Software Engineer',
-		description: 'Freelancer.com · Sydney, NSW · Mar. 2022 - Now',
-		image: '/images/freelancer-logo.jpg',
-		imageAlt: 'Freelancer.com logo',
-	},
-	{
-		title: 'Software Engineer Intern',
-		description: 'Freelancer.com · Sydney, NSW · Dec. 2021 - Feb 2022',
-		image: '/images/freelancer-logo.jpg',
-		imageAlt: 'Freelancer.com logo',
-	},
-];
-
 type WorkHistoryProps = {
 	workHistoryItems: HomePageContentQuery['works'];
 };
